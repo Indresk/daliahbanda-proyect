6e14e7d0e4e43f7f925bdf878e970f7c6601325f
Indresk <gif.poto@gmail.com>
2026-02-04 23:27:32 -0500

    SEO test v6

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -4,9 +4,10 @@ import { routes } from './routes'
 import { metaIndex } from './meta/metaIndex'
 
 let head = ''
+let metaFlag = true
 const getHead = (pathname) => {
   if (pathname === '/') return metaIndex
-  
+  metaFlag = false
   return {
     title: "Daliah Banda",
     description: "Sitio oficial Daliah Banda"
@@ -30,42 +31,47 @@ export async function render(req){
 
   const pathname = new URL(fullUrl).pathname
   const meta = getHead(pathname)
-  head = `
-    <title>${meta.title}</title>
-    <meta name="description" content="${meta.description}">
-    <meta name="keywords" content="${meta.keywords}">
-    <meta name="author" content="${meta.author}">
-    <link rel="canonical" href="${meta.canonical}">
-    
-    <!-- OG -->
-    <meta property="og:type" content="${meta.og.type}">
-    <meta property="og:title" content="${meta.og.title}">
-    <meta property="og:description" content="${meta.og.description}">
-    <meta property="og:url" content="${meta.og.url}">
-    <meta property="og:site_name" content="${meta.og.siteName}">
-    <meta property="og:locale" content="${meta.og.locale}">
+  if(metaFlag){
+    head = `
+      <title>${meta.title}</title>
+      <meta name="description" content="${meta.description}">
+      <meta name="keywords" content="${meta.keywords}">
+      <meta name="author" content="${meta.author}">
+      <link rel="canonical" href="${meta.canonical}">
+      
+      <!-- OG -->
+      <meta property="og:type" content="${meta.og.type}">
+      <meta property="og:title" content="${meta.og.title}">
+      <meta property="og:description" content="${meta.og.description}">
+      <meta property="og:url" content="${meta.og.url}">
+      <meta property="og:site_name" content="${meta.og.siteName}">
+      <meta property="og:locale" content="${meta.og.locale}">
 
-    <meta property="og:image" content="${meta.og.image}">
-    <meta property="og:image:width" content="${meta.og.imageWidth}">
-    <meta property="og:image:height" content="${meta.og.imageHeight}">
-    <meta property="og:image:alt" content="${meta.og.imageAlt}">
+      <meta property="og:image" content="${meta.og.image}">
+      <meta property="og:image:width" content="${meta.og.imageWidth}">
+      <meta property="og:image:height" content="${meta.og.imageHeight}">
+      <meta property="og:image:alt" content="${meta.og.imageAlt}">
 
-    <meta name="twitter:card" content="${meta.twitter.card}">
-    <meta name="twitter:title" content="${meta.twitter.title}">
-    <meta name="twitter:description" content="${meta.twitter.description}">
-    <meta name="twitter:image" content="${meta.twitter.image}">
-    <meta name="twitter:site" content="${meta.twitter.site}">
-    <meta name="twitter:creator" content="${meta.twitter.creator}">
+      <meta name="twitter:card" content="${meta.twitter.card}">
+      <meta name="twitter:title" content="${meta.twitter.title}">
+      <meta name="twitter:description" content="${meta.twitter.description}">
+      <meta name="twitter:image" content="${meta.twitter.image}">
+      <meta name="twitter:site" content="${meta.twitter.site}">
+      <meta name="twitter:creator" content="${meta.twitter.creator}">
 
-    <meta name="music:genre" content="${meta.music.genre}">
-    <meta name="music:musician" content="${meta.music.musician}">
-    <meta name="music:location" content="${meta.music.location}">
-    
-    <!-- JSON-LD -->
-    <script type="application/ld+json">
-      ${JSON.stringify(meta.schema)}
-    </script>
-  `
+      <meta name="music:genre" content="${meta.music.genre}">
+      <meta name="music:musician" content="${meta.music.musician}">
+      <meta name="music:location" content="${meta.music.location}">
+      
+      <!-- JSON-LD -->
+      <script type="application/ld+json">
+        ${JSON.stringify(meta.schema)}
+      </script>`}
+  else{
+    head = `
+      <title>${meta.title}</title>
+      <meta name="description" content="${meta.description}">`
+  }
 
   return { 
     html,
