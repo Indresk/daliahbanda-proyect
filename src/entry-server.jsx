5eb1a8f8c6dddcc2d9e904839d4201d6f8d95314
Indresk <gif.poto@gmail.com>
2026-02-04 23:19:53 -0500

    SEO test v5

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -38,16 +38,34 @@ export async function render(req){
     <link rel="canonical" href="${meta.canonical}">
     
     <!-- OG -->
+    <meta property="og:type" content="${meta.og.type}">
     <meta property="og:title" content="${meta.og.title}">
     <meta property="og:description" content="${meta.og.description}">
+    <meta property="og:url" content="${meta.og.url}">
+    <meta property="og:site_name" content="${meta.og.siteName}">
+    <meta property="og:locale" content="${meta.og.locale}">
+
     <meta property="og:image" content="${meta.og.image}">
+    <meta property="og:image:width" content="${meta.og.imageWidth}">
+    <meta property="og:image:height" content="${meta.og.imageHeight}">
+    <meta property="og:image:alt" content="${meta.og.imageAlt}">
+
+    <meta name="twitter:card" content="${meta.twitter.card}">
+    <meta name="twitter:title" content="${meta.twitter.title}">
+    <meta name="twitter:description" content="${meta.twitter.description}">
+    <meta name="twitter:image" content="${meta.twitter.image}">
+    <meta name="twitter:site" content="${meta.twitter.site}">
+    <meta name="twitter:creator" content="${meta.twitter.creator}">
+
+    <meta name="music:genre" content="${meta.music.genre}">
+    <meta name="music:musician" content="${meta.music.musician}">
+    <meta name="music:location" content="${meta.music.location}">
     
     <!-- JSON-LD -->
     <script type="application/ld+json">
       ${JSON.stringify(meta.schema)}
     </script>
   `
-  
 
   return { 
     html,
