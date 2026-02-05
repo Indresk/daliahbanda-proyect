a46b5121c995cfc4103facc91cce0908fc0f367a
Mike_ladino <mike18117@gmail.com>
2026-02-05 02:12:25 -0500

    ruta del asset actualizada

--- src/components/DaliahClicker/DaliahClicker.jsx
+++ src/components/DaliahClicker/DaliahClicker.jsx
@@ -14,7 +14,7 @@ const DaliahClicker=()=>{
   }, [daliahCookieCount]);
 return(
     <div class="container">
-        <a class="cookie-button"href="#"  onClick={()=>{setDaliahCookieCount(daliahCookieCount+1)}}><img src=".\public\DaliahLogoWhiteIcon.ico" width="100px"  alt="" /></a>
+        <a class="cookie-button"href="#"  onClick={()=>{setDaliahCookieCount(daliahCookieCount+1)}}><img src="\DaliahLogoWhiteIcon.ico" width="100px"  alt="" /></a>
         <h2>Daligalletas: {daliahCookieCount}</h2>
     </div>
 );
