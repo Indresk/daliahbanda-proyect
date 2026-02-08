c10c7a68abb9b970457be178c60efd8162e0644a
Indresk <gif.poto@gmail.com>
2026-02-08 13:30:56 -0500

    ajuste de liveBanner

--- src/components/global/Header/Header.jsx
+++ src/components/global/Header/Header.jsx
@@ -1,10 +1,15 @@
 import { Link, useLocation } from 'react-router-dom'
 
+import LiveBanner from '../LiveBanner/LiveBanner'
+
 export default function Header(){
     const location = useLocation()
+    let verification = false
+
   
     return (
         <header className='bg-black'>
+            {verification&&<LiveBanner/>}
             <nav>
                 <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
                 <Link to="/test" className={location.pathname === '/test' ? 'active' : ''}>Test</Link>
---
+++ src/components/global/LiveBanner/LiveBanner.jsx
@@ -0,0 +1,7 @@
+export default function LiveBanner(){
+    return(
+        <div className="bg-red-600 w-full">
+            <h3>Live banner</h3>
+        </div>
+    )
+}
