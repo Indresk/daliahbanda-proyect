ef3c861b39c2cfa9456d06a3e27d3ececb4d8b61
Indresk <gif.poto@gmail.com>
2026-02-15 14:22:52 -0500

    ajuste de live status

--- src/context/LiveContext.jsx
+++ src/context/LiveContext.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+import {getLiveStatus} from '../../../../api/webhooks/kick/getSubscription.js'
 
 export const LiveContext = createContext();
 
 function LiveProvider({children}){
-    const [liveStatus, setLiveStatus] = useState(false)
+    const [liveStatus, setLiveStatus] = useState(getLiveStatus())
 
     return(
         <LiveContext.Provider value={{liveStatus:liveStatus,setLiveStatus:setLiveStatus}}>
