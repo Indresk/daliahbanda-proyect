367d5084393854986cea538e9928bd54d225644f
Indresk <gif.poto@gmail.com>
2026-04-12 18:42:05 -0500

    align fix

--- src/views/GalleryView.jsx
+++ src/views/GalleryView.jsx
@@ -22,34 +22,18 @@ export default function GalleryView() {
 
 		getImages();
 	}, []);
-const filters = [
-	{name: "todos",
-		link:"/gallery"
-	},
-	{name: "Silvia",
-		link:"/gallery/silvia"
-	},
-	{name: "Mike",
-		link:"/gallery/mike"
-	},
-	{name: "Rafa",
-		link:"/gallery/rafa"
-	},
-	{name: "Jesus",
-		link:"/gallery/jesus"
-	},
-	{name: "Chris",
-		link:"/gallery/chris"
-	},
-	{name: "Nico",
-		link:"/gallery/nico"
-	}
-	
-]
+	const filters = [
+		{ name: 'todos', link: '/gallery' },
+		{ name: 'Silvia', link: '/gallery/silvia' },
+		{ name: 'Mike', link: '/gallery/mike' },
+		{ name: 'Rafa', link: '/gallery/rafa' },
+		{ name: 'Jesus', link: '/gallery/jesus' },
+		{ name: 'Chris', link: '/gallery/chris' },
+		{ name: 'Nico', link: '/gallery/nico' },
+	];
 	return (
 		<>
-			
-		{/* <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+			{/* <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
 			<div className='grid gap-4'>
 			{images.map((link, i) => (
 				<div key={i} >
@@ -68,32 +52,32 @@ const filters = [
 			))}
 			</div>
 		</div> */}
-		<section className='px-8 md:p-14 min-h-[440px]'>
-				<h1 className='text-8xl uppercase '>Eventos <br /> <strong>En vivo</strong></h1>
-				
-
+			<section className='px-8 md:p-14 min-h-[440px]'>
+				<h1 className='text-8xl uppercase '>
+					Eventos <br /> <strong>En vivo</strong>
+				</h1>
 			</section>
-			<section className='searcher flex gap-4 px-8 md:p-14'>
+			<section className='searcher flex gap-4 px-8 md:p-14 items-center'>
 				<div>Filtros:</div>
-				
-					{filters.map((filter)=>(
-						<NavLink  key={filter.name} to={filter.link}>
-							
-							{({isActive})=> isActive?<Button variant="primary" className=''>
-								{filter.name}
-							</Button>:<Button variant="secondary" className=''>
-								{filter.name}
-							</Button>}
-							
-						</NavLink>
 
-
-					)				
-				)}
-				
+				{filters.map((filter) => (
+					<NavLink key={filter.name} to={filter.link}>
+						{({ isActive }) =>
+							isActive ? (
+								<Button variant='primary' className=''>
+									{filter.name}
+								</Button>
+							) : (
+								<Button variant='secondary' className=''>
+									{filter.name}
+								</Button>
+							)
+						}
+					</NavLink>
+				))}
 			</section>
 			<>
-			<Outlet/>
+				<Outlet />
 			</>
 		</>
 	);
